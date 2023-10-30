559ffb071273f5ff5a242def05ddef9c6454923f
Elvin <ELVINLA.22@ichat.sp.edu.sg>
2023-10-30 22:36:10 +0800

    exercise 2 test functions

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,30 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_length_equal_or_greater_than_10():
+    result = []
+    input_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    expected_result = 1
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == expected_result)
+
+def test_bubble_sort_length_equal_0():
+    result = []
+    input_arr = []
+
+    expected_result = 0
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == expected_result)
+
+def test_bubble_sort_not_integer():
+    result = []
+    input_arr = [1, 2, 3.5, 4, 5, 6, 7, 8, 9]
+
+    expected_result = 2
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == expected_result)
